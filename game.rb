6fbcb20bd8da721d9864f506f4db61913754d064
letsleaveitempty <anita.jaszewska@gmail.com>
2014-06-30 09:54:22 +0200

    rubygame

---
+++ game.rb
@@ -0,0 +1,37 @@
+require 'rubygems'
+require 'rubygame'
+ 
+class Game
+    def initialize
+        @screen = Rubygame::Screen.new [640, 480], 0, [Rubygame::HWSURFACE, Rubygame::DOUBLEBUF]
+        @screen.title = 123
+ 
+        @queue = Rubygame::EventQueue.new
+        @clock = Rubygame::Clock.new
+        @clock.target_framerate = 60
+    end
+ 
+    def run!
+        loop do
+            update
+            draw
+            @clock.tick
+        end
+    end
+ 
+    def update
+        @queue.each do |ev|
+            case ev
+                when Rubygame::QuitEvent
+                    Rubygame.quit
+                    exit
+            end
+        end
+    end
+ 
+    def draw
+    end
+end
+ 
+g = Game.new
+g.run!

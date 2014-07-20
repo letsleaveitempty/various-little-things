489d33ed50d8f5845fe7d7ec096e1f7f75daf9c0
letsleaveitempty <anita.jaszewska@gmail.com>
2014-07-20 20:50:13 +0200

    added dirctionary

--- hangman1.rb
+++ hangman1.rb
@@ -3,10 +3,6 @@ def start_hangman
   quit_or_play
 end
 
-def dictionary
-  %w(one two three four five)
-end
-
 def quit_or_play
   puts "Type any key to play, \"quit\" to... quit :)\n"
   print "> "
@@ -96,6 +92,10 @@ def check_letters_to_guess
     puts "\n\nNOPE!\n\n"
     play_game
   end 
+end
+
+def dictionary
+  %w(Adult Aeroplane Air Aircraft Airforce Airport Album Alphabet Apple Arm Army Baby Baby Backpack Balloon Banana Bank Barbecue Bathroom Bathtub Bed Bed Bee Bible Bible Bird Bomb Book Boss Bottle Bowl Box Boy Brain Bridge Butterfly Button Cappuccino Car Carpet Carrot Cave Chair Chess Chief Child Chisel Chocolates Church Church Circle Circus Circus Clock Clown Coffee Comet Compact Compass Computer Crystal Cup Cycle Database Desk Diamond Dress Drill Drink Drum Dung Ears Earth Egg Electricity Elephant Eraser Explosive Eyes Family Fan Feather Festival Film Finger Fire Floodlight Flower Foot Fork Freeway Fruit Fungus Game Garden Gas Gate Gemstone Girl Gloves God Grapes Guitar Hammer Hat Hieroglyph Highway Horoscope Horse Hose Ice Insect Jet Junk Kaleidoscope Kitchen Knife Leather Leg Library Liquid Magnet Man Map Maze Meat Meteor Microscope Milk Milkshake Mist Money Monster Mosquito Mouth Nail Navy Necklace Needle Onion PaintBrush Pants Parachute Passport Pebble Pendulum Pepper Perfume Pillow Plane Planet Pocket Post Potato Printer Prison Pyramid Radar Rainbow Record Restaurant Rifle Ring Robot Rock Rocket Roof Room Rope Saddle Salt Sandpaper Sandwich Satellite School Ship Shoes Shop Shower Signature Skeleton Slave Snail Software Solid Space Spectrum Sphere Spice Spiral Spoon Sport Spot Square Staircase Star Stomach Sun Sunglasses Surveyor Swimming Pool Sword Table Tapestry Teeth Telescope Television Tennis Thermometer Tiger Toilet Tongue Torch Torpedo Train Treadmill Triangle Tunnel Typewriter Umbrella Vacuum Vampire Videotape Vulture Water Weapon Web Wheelchair Window Woman Worm)
 end
 
 start_hangman
